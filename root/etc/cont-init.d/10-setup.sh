ad4b08f5beb9df1112d5903c83c617997e38f34a
Luca Critelli <lucacri@gmail.com>
2018-11-21 16:57:20 -0500

    Allow port change

--- root/etc/cont-init.d/10-setup.sh
+++ root/etc/cont-init.d/10-setup.sh
@@ -8,5 +8,6 @@ if [ ! -f /app/laravel-echo-server.json ]; then
     sed -i "s|REDIS_PORT|${LARAVEL_ECHO_REDIS_PORT:-6379}|i" /app/laravel-echo-server.json
     sed -i "s|LARAVEL_ECHO_SERVER_DEBUG|${LARAVEL_ECHO_SERVER_DEBUG:-false}|i" /app/laravel-echo-server.json
     sed -i "s|LARAVEL_ECHO_AUTH_HOST|${LARAVEL_ECHO_AUTH_HOST:-http\:\/\/localhost}|i" /app/laravel-echo-server.json
-    sed -i "s|REDIS_DB_BACKEND|${LARAVEL_ECHO_AUTH_HOST:-0}|i" /app/laravel-echo-server.json
+    sed -i "s|REDIS_DB_BACKEND|${LARAVEL_ECHO_REDIS_DB_BACKEND:-0}|i" /app/laravel-echo-server.json
+    sed -i "s|LARAVEL_ECHO_SERVER_PORT|${LARAVEL_ECHO_SERVER_PORT:-6001}|i" /app/laravel-echo-server.json
 fi
